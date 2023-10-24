ae033d2d7e054cd85491cb840cd669fd6805cf6a
Helian244 <201900828@est.umss.edu>
2023-10-24 01:57:14 -0400

    Ultimos Arreglos Interfaz 6

--- Documents/NetBeansProjects/Sprint/src/main/java/Interfaz/RegistroEstudiantes.java
+++ Documents/NetBeansProjects/Sprint/src/main/java/Interfaz/RegistroEstudiantes.java
@@ -9,6 +9,7 @@ package Interfaz;
  * @author Usuario
  */
 import Codigo.Conexion;
+import Interfaz.MenuPrincipal;
 import java.sql.CallableStatement;
 import javax.swing.JOptionPane;
 import java.sql.Connection;
@@ -21,12 +22,14 @@ import java.sql.Date;
 public class RegistroEstudiantes extends javax.swing.JFrame {
     Conexion objetoCon = new Conexion();
     Connection conectar = objetoCon.establecerConexion();
+    MenuPrincipal men;
     /**
      * Creates new form RegistroEstudiantes
      */
-    public RegistroEstudiantes() {
-        this.setLocationRelativeTo(null);
+    public RegistroEstudiantes(MenuPrincipal men) {
+        this.men = men;
         initComponents();
+        this.setLocationRelativeTo(null);
     }
     public int existeEstudiante(String nom, String apP, String apM,
                                 String ci, String dia, String mes, String anio){
@@ -149,9 +152,15 @@ public class RegistroEstudiantes extends javax.swing.JFrame {
         anio = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
 
         can.setFont(new java.awt.Font("DialogInput", 1, 12)); // NOI18N
         can.setText("Cancelar");
+        can.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                canActionPerformed(evt);
+            }
+        });
 
         Ing.setFont(new java.awt.Font("DialogInput", 1, 12)); // NOI18N
         Ing.setText("Ingresar");
@@ -366,40 +375,45 @@ public class RegistroEstudiantes extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_anioActionPerformed
 
+    private void canActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_canActionPerformed
+        men.setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_canActionPerformed
+
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(RegistroEstudiantes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(RegistroEstudiantes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(RegistroEstudiantes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(RegistroEstudiantes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new RegistroEstudiantes().setVisible(true);
-            }
-        });
-    }
+//    public static void main(String args[]) {
+//        /* Set the Nimbus look and feel */
+//        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+//        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+//         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+//         */
+//        try {
+//            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+//                if ("Nimbus".equals(info.getName())) {
+//                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+//                    break;
+//                }
+//            }
+//        } catch (ClassNotFoundException ex) {
+//            java.util.logging.Logger.getLogger(RegistroEstudiantes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (InstantiationException ex) {
+//            java.util.logging.Logger.getLogger(RegistroEstudiantes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (IllegalAccessException ex) {
+//            java.util.logging.Logger.getLogger(RegistroEstudiantes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+//            java.util.logging.Logger.getLogger(RegistroEstudiantes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        }
+//        //</editor-fold>
+//
+//        /* Create and display the form */
+//        java.awt.EventQueue.invokeLater(new Runnable() {
+//            public void run() {
+//                new RegistroEstudiantes().setVisible(true);
+//            }
+//        });
+//    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton Ing;
